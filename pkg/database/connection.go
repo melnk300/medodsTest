acf07bd8310097ae1857dc454183e672e57dc7d6
Руслан Мельник <melnk300@gmail.com>
2024-08-16 20:29:17 +0500

    FIX: added host param

--- pkg/database/connection.go
+++ pkg/database/connection.go
@@ -9,12 +9,14 @@ import (
 )
 
 func MakeConnection() *sql.DB {
+	dbHost := os.Getenv("POSTGRESS_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
 	dbUser := os.Getenv("POSTGRES_USER")
 	dbPassword := os.Getenv("POSTGRES_PASSWORD")
 	dbName := os.Getenv("POSTGRES_DB")
 
-	dsn := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		dbUser, dbPassword, dbName)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
